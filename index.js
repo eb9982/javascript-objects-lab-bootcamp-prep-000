d9c1be6e08c903318271e0b7680fedeaf61a4488
Luke Borsare <lborsare1@gmail.com>
2017-09-12 23:04:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object,key) {
-  delete object[key]
+  const object2 = object
+  delete object2[key]
   return object
 }
